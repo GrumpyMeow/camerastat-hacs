0da7ab5bceb8e3135f7ea66aa6ade84967e145c6
Meow <sander.schutten@gmail.com>
2023-07-24 16:35:54 +0200

    Update __init__.py

--- custom_components/camerastat/__init__.py
+++ custom_components/camerastat/__init__.py
@@ -1,6 +1,6 @@
 """The Camera Statistic integration."""
 from __future__ import annotations
-from homeassistant.components.camerastat.coordinator import CameraStatCoordinator
+from .coordinator import CameraStatCoordinator
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
